9982871665f71de7e914d2756b1bf5e37136a776
boooyeon <boooyeon@gmail.com>
2019-07-02 14:36:40 +0900

    first_boyeon

---
+++ README.md
@@ -0,0 +1,41 @@
+# dobby_front
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Run your tests
+```
+npm run test
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### **Views**
+
+* SignIn
+* SignUP
+* ManageHome
+* ManageDetail
+* MoveIn
+* InforPage
+* Chat
+* Tenant
+
---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="SiginIn">
+    <el-header style="backgroundColor:blue;">
+      <el-row>
+        <el-col span="1" offset="0">로고</el-col>
+        <el-col span="3" offset="15">
+          <el-input placeholder="Id" v-model="id" clearable></el-input>
+        </el-col>
+        <el-col span="3" offset>
+          <el-input placeholder="Password" v-model="password" clearable></el-input>
+        </el-col>
+        <el-col span="1">
+          <el-button>Login</el-button>
+        </el-col>
+      </el-row>
+    </el-header>
+    <el-main></el-main>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      id: "",
+      password: ""
+    };
+  }
+};
+</script>
+
+<style lang='scss' scoped>
+</style>
