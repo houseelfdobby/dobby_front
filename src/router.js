36c8263faf9bc72b9dc50abe5953aba53de6c788
boooyeon <boooyeon@gmail.com>
2019-07-03 17:17:49 +0900

    ecn: SignUpHouseOwne

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import SignUpTenant from './views/SignUpTenant'
+import SignUpHouseOwner from './views/SignUpHouseOwner'
 
 Vue.use(Router)
 
@@ -12,6 +14,16 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/signup_tenant',
+      name: 'signup_tenant',
+      component: SignUpTenant
+    },
+    {
+      path: '/signup_houseowner',
+      name: 'signup_houseowner',
+      component: SignUpHouseOwner
     }
   ]
 })
---
+++ src/views/SignUpHouseOwner.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="SignUp">
+    <div class="SignUp-Cotent">
+      <div class="SignUp-Banner">
+        <div class="SignUp-BannerImg">
+          <img src="@/assets/도비.png" width="50" height="50" alt />
+        </div>
+        <div class="SignUp-BannerText">- HOUSE OWNER -</div>
+        <br>
+      </div>
+      <div class="SignUp-Form" align="left" >
+        <div class="SignUp-FormItem">
+          <div class="Name">
+          <div class="SignUp-FormLabel">이름</div>
+          <input type="text" class="SignUp-FormTextbox"/>
+          </div>
+          <div class="PassWord">
+          <div class="SignUp-FormLabel">비밀번호</div>
+          <input type="text" class="SignUp-FormTextbox"/>
+          <div class="SignUp-FormLabel">비밀번호 확인</div>
+          <input type="text" class="SignUp-FormTextbox"/>
+          </div>
+          <div class="Address">
+          <div class="SignUp-FormLabel">건물주소</div>
+          <input type="text" class="SignUp-FormTextbox" style="width:140px;" placeholder="우편 번호"/> &nbsp; 
+          <button type="submit" class="SignUp-FormButton" style="width:75px;">검색</button>
+          <input type="text" class="SignUp-FormTextbox" placeholder="상세 주소"/>
+          </div>
+          <div class="Account">
+          <div class="SignUp-FormLabel">계좌번호</div>
+          <select name="bank">
+            <option value selected disabled hidden>==은행==</option>
+            <option value="광주은행">광주은행</option>
+            <option value="전북은행">전북은행</option>
+            <option value="농협">농협</option>
+          </select> &nbsp;
+          <input type="text" class="SignUp-FormTextbox" style="width:135px;" placeholder="계좌 번호" />
+          </div>
+          <div class="Building">        
+            <div class="SignUp-FormLabel">건물상세정보</div>
+            <input type="text" class="SignUp-FormTextbox" style="width:105px;" placeholder="층"> &nbsp;
+            <input type="text" class="SignUp-FormTextbox" style="width:105px;" placeholder="호"/>
+          </div>
+          <br>
+          <button type="submit" class="SignUp-FormButton">가입하기</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.SignUp {
+  font-family: Arial, Helvetica, sans-serif;
+  background-color: rgb(217, 217, 217);
+  &-Content {
+  }
+  &-BannerImg {
+  }
+  &-Form {
+    width: 100%;
+    max-width: 300px;
+    margin-left: 290px;
+    &Textbox{
+      width:230px;
+      }
+    &Button{
+      background-color: rgb(32,56,100);
+      color: white;
+      width: 234px;
+    }
+   
+  }
+  
+}
+
+</style>
+
+
