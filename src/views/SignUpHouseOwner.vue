150bb3425a9d8ccafa7dea4e9e1bc6958a7dfea5
lebmouse <gtah2yk@gmail.com>
2019-07-11 15:38:04 +0900

    ref: index.scss

--- src/views/SignUpHouseOwner.vue
+++ src/views/SignUpHouseOwner.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="SignUp">
+  <div class="Owner SignUp">
     <div class="SignUp-Content">
       <div class="SignUp-Banner">
         <img class="SignUp-BannerImg" src="@/assets/image/도비.png" alt />
@@ -8,7 +8,7 @@
       <div class="SignUp-Form">
         <div class="SignUp-FormItem" v-for="(item, index) in formItems" :key="'basicform'+index">
           <div class="SignUp-FormLabel">{{item.label}}</div>
-          <el-input class="SignUp-FormTextInput"></el-input>
+          <el-input class="SignUp-FormInput"></el-input>
         </div>
         <div class="SignUp-FormItem">
           <div class="SignUp-FormLabel">건물주소</div>
@@ -39,7 +39,6 @@
         </div>
         <input type="button" class="SignUp-FormEnroll" value="가입하기" />
       </div>
-      <!-- 건물주 Main으로  -->
     </div>
   </div>
 </template>
@@ -68,43 +67,13 @@ export default {
 <style lang="scss" scoped>
 @import "@/assets/css/index.scss";
 .SignUp {
-  font-family: a스마일B;
-  background-color: rgb(217, 217, 217);
-  height: 100%;
-  &-Content {
-    width: 100%;
-    max-width: 350px;
-    margin: auto;
-  }
-  &-Banner {
-    padding-top:35px;
-    text-align: center;
-    &Img{
-      width:55px;
-    }
-  }
-
   &-Form {
-    &TextInput {
-      margin: 5px auto;
-    }
     &Address {
       display: grid;
       grid-gap: 7px;
       grid-template-areas:
         "zip zip button"
         "detail detail detail";
-    }
-    &Enroll {
-      font-family: a스마일B;
-      margin-top: 50px;
-      background-color: rgb(32, 56, 100);
-      color: white;
-      padding: 10px;
-      width: 100%;
-      // height: 22px;
-      border: 1px solid rgb(32, 56, 100);
-      border-radius: 3px;
     }
   }
 }
--- src/views/SignUpTenant.vue
+++ src/views/SignUpTenant.vue
@@ -1,25 +1,16 @@
 <template>
-  <div class="SignUp">
+  <div class="Tenant SignUp">
     <div class="SignUp-Content">
       <div class="SignUp-Banner">
-        <div class="SignUp-BannerImg">
-          <img class="logo" src="@/assets/image/도비.png" alt />
-        </div>
+        <img class="SignUp-BannerImg" src="@/assets/image/도비.png" alt />
         <div class="SignUp-BannerText">- TENANT -</div>
-        <br />
       </div>
       <div class="SignUp-Form">
-        <div
-          class="name SignUp-FormItem"
-          v-for="(formItem,index) in formList"
-          :key="'singup'+index"
-        >
+        <div class="SignUp-FormItem" v-for="(formItem,index) in formList" :key="'singup'+index">
           <div class="SignUp-FormLabel" v-text="formItem.name"></div>
-          <el-input class="SignUp-FormTextbox" :type="formItem.type" v-model="formItem.value"></el-input>
+          <el-input class="SignUp-FormInput" :type="formItem.type" v-model="formItem.value"></el-input>
         </div>
-        <div class="SignUp-FormItem">
-          <input type="button" class="SignUp-FormEnroll" value="가입하기" />
-        </div>
+        <input type="button" class="SignUp-FormEnroll" value="가입하기" />
       </div>
     </div>
   </div>
@@ -57,39 +48,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.SignUp {
-  font-family: a스마일B;
-  background-color: rgb(217, 217, 217);
-  height: 100%;
-  text-align: center;
-  &-BannerImg {
-    .logo {
-      width: 50px;
-      height: 50px;
-    }
-  }
-  &-Form {
-    width: 100%;
-    max-width: 800px;
-    margin: auto;
-    &Item {
-      margin: auto;
-      max-width: 500px;
-      padding: 0px 0px 8px 0px;
-    }
-    &Textbox {
-      height: 22px;
-      border: 1px solid #999999;
-    }
-    &Enroll {
-      margin-top: 25px;
-      width: 100%;
-      background-color: rgb(32, 56, 100);
-      color: white;
-      font-weight: 550;
-      padding: 10px;
-      border: none;
-    }
-  }
-}
+@import "@/assets/css/index.scss";
 </style>
